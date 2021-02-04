e199e2c3135b86bc9bf3fc080d0e96ee33d7c9ee
Marco Brandizi <marco.brandizi@gmail.com>
2021-02-04 18:53:10 +0000

    Bug in GH Actions script.

--- ci-build/build-begin.sh
+++ ci-build/build-begin.sh
@@ -1,3 +1,5 @@
+[[ -z "$SECRETS_GIST_TOKEN" ]] && return
+
 echo -e "\n\n\tDownloading credentials\n"
 gist_url="https://gist.githubusercontent.com/marco-brandizi/$SECRETS_GIST_TOKEN/raw/github-brandizi-secrets.sh"
 source <(wget -O - "$gist_url")
